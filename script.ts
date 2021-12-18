c8cd885cc76a48615715ac3df531e6e989628b93
Meike Madel <meike.luise.madel@hs-furtwangen.de>
2021-12-18 09:50:32 +0100

    update sinnlos

--- script.ts
+++ script.ts
@@ -10,5 +10,12 @@ namespace script {
         let buttonWeiter: HTMLButtonElement = document.createElement("button");
         buttonWeiter.appendChild(document.createTextNode("zurück"));
         divButton.appendChild(buttonWeiter);
+
+        let divButton2: HTMLElement = document.getElementById("buttonWeiter");
+        let buttonWeiter2: HTMLButtonElement = document.createElement("button");
+        buttonWeiter2.appendChild(document.createTextNode("zurück"));
+        divButton2.appendChild(buttonWeiter);
+
+
      
     }
