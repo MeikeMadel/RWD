e44d662140d3448e91f716dfd19fea795f3d4f15
Meike Madel <meike.luise.madel@hs-furtwangen.de>
2021-12-22 13:18:55 +0100

    Update desktop

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
+var RWD;
+(function (RWD) {
+    let grafikklein = document.getElementById("kreis-grafik-klein");
+    grafikklein.addEventListener("click", function () {
+        grafikklein.classList.add("aktiv");
+    });
+    let grafikgross = document.getElementById("kreis-grafik-groß");
+    grafikgross.addEventListener("click", function () {
+        grafikgross.classList.add("aktiv");
+    });
+})(RWD || (RWD = {}));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -4,6 +4,17 @@ declare var jquery: any;
      
 namespace RWD {
 
+  let grafikklein: HTMLElement = document.getElementById("kreis-grafik-klein");
+
+  grafikklein.addEventListener("click", function() {
+    grafikklein.classList.add("aktiv");
+  });
+
+  let grafikgross: HTMLElement = document.getElementById("kreis-grafik-groß");
+
+  grafikgross.addEventListener("click", function() {
+    grafikgross.classList.add("aktiv");
+  });
 
   
   
