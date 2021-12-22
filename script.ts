358a9b58ebc1b8dcc0b04c34a8471ce7379414ff
Meike Madel <meike.luise.madel@hs-furtwangen.de>
2021-12-22 12:46:05 +0100

    update

--- script.ts
+++ script.ts
@@ -1,21 +1,10 @@
-namespace script { 
 
-
-        document.getElementById("dogfood").animate([
-          {transform: "translate(0)px"},
-          {transform: "translate(-300px)"}
-        ]);
-
-        let divButton: HTMLElement = document.getElementById("buttonZurück");
-        let buttonWeiter: HTMLButtonElement = document.createElement("button");
-        buttonWeiter.appendChild(document.createTextNode("zurück"));
-        divButton.appendChild(buttonWeiter);
-
-        let divButton2: HTMLElement = document.getElementById("buttonWeiter");
-        let buttonWeiter2: HTMLButtonElement = document.createElement("button");
-        buttonWeiter2.appendChild(document.createTextNode("zurück"));
-        divButton2.appendChild(buttonWeiter);
-
+declare var jquery: any; 
 
      
-    }
+namespace RWD {
+
+
+  
+  
+}
